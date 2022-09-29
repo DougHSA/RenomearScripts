307ddda73bfd092152f793ef482034245abee00a
douglas <douglas.henrique@brunsker.com.br>
2022-09-29 17:54:48 -0300

    first commit

---
+++ RenomearScriptsBanco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RenomearScriptsBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"..\CRIAÇÃO DO BANCO\LADO BRUNSKER\PKG'S";
+
+            string targetPath = sourcePath + @"\Script\ScriptCompleto.txt";
+
+            Console.WriteLine("Digite o banco do Cliente: (Exemplo: BSGLA)");
+            string bancoCliente = Console.ReadLine().ToUpper();
+            FileInfo criadoEm;
+            try
+            {
+                Directory.CreateDirectory(sourcePath + @"\Script");
+                var arquivosExistentes = Directory.EnumerateFiles(sourcePath + @"\Script","*.txt");
+                if (arquivosExistentes !=null)
+                    File.Delete(targetPath);
+                
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    
+                    var files = Directory.EnumerateFiles(sourcePath, "*.txt");
+                    foreach (var file in files)
+                    {
+                        string[] lines = lines = File.ReadAllLines(file);
+
+                        foreach (string line in lines)
+                        {
+                            sw.WriteLine(line.Replace("BSGLA", bancoCliente));
+                        }
+                    }
+                    criadoEm = new FileInfo(targetPath);
+                }
+                Console.WriteLine($"Arquivo gerado em {criadoEm.FullName}");
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ RenomearScriptsBanco/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("RenomearScriptsBanco")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RenomearScriptsBanco")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("374a24b7-b9f3-4b57-b6e9-f46d5cd9a1ae")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
